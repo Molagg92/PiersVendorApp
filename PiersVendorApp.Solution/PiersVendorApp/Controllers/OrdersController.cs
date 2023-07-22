bedf248c23358d0311df8d3bf190ad48c8a3700d
Erik Z <molagg92@gmail.com>
2023-07-22 15:23:38 -0700

    will start to build out the OrdersController

---
+++ PiersVendorApp.Solution/PiersVendorApp/Controllers/OrdersController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using PiersVendorApp.Models;
+using System.Collections.Generic;
+
+namespace PiersVendorApp.Controllers
+{
+  public class OrderRequestController : Controller
+  {
+    
+  }
+}
--- PiersVendorApp.Solution/PiersVendorApp/Controllers/VendorController.cs
+++ PiersVendorApp.Solution/PiersVendorApp/Controllers/VendorController.cs
@@ -11,9 +11,32 @@ namespace PiersVendorApp.Controllers
     [HttpGet("/vendors")]
     public ActionResult Index()
     {
-      List<VendorAccount> allVendors = VendorAccount.GetAll();
-      return View(allVendors);
+        List<VendorAccount> allVendors = VendorAccount.GetAll();
+        return View(allVendors);
     }
+
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+        return View();
+    }
+
+      [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName)
+    {
+        VendorAccount newAccount = new VendorAccount(vendorName);
+        return RedirectToAction("Index");
+    }
+    // [HttpGet("/categories/{id}")]
+    // public ActionResult Show(int id)
+    // {
+    //   Dictionary<string, object> model = new Dictionary<string, object>();
+    //   VendorAccount selectedVendor = VendorAccount.Find(id);
+    //   List<OrderRequest> vendorOrder = selectedVendor.Orders;
+    //   model.Add("testvendor", selectedVendor);
+    //   model.Add("testorder", vendorOrder);
+    //   return View(model);
+    // }
 
     }
 }
